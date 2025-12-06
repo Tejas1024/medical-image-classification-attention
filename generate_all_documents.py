751796dfc41c70aa6ebab99d7ae7d4fb71b2a7cf
Tejas Gowda T R <152372122+Tejas1024@users.noreply.github.com>
2025-12-06 20:17:35 +0530

    Create generate_all_documents.py

---
+++ generate_all_documents.py
@@ -0,0 +1,8 @@
+"""Comprehensive Research Paper and Documentation Generator
+
+Generates all required documents for journal submission
+
+Author: Research Implementation
+Date: December 2025
+Requirements: pip install reportlab pillow
+"""
